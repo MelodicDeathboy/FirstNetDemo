3085fc267f2968bab84d5af9689ce08b37839bd2
MelodicDeathboy <2632431654@qq.com>
2020-06-11 22:59:11 +0800

    Update Program.cs
    add code

--- NetDemo/Program.cs
+++ NetDemo/Program.cs
@@ -7,6 +7,7 @@ namespace NetDemo
         static void Main(string[] args)
         {
             Console.Write(1);
+            Console.Write(1);
             Console.WriteLine("Hello World!");
         }
     }
