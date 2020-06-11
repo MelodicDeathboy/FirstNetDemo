387b4a6523284d62714338ced410cc05670f546b
2632431654@qq.com <Guoguo123>
2020-06-11 22:48:31 +0800

    init code

---
+++ NetDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
